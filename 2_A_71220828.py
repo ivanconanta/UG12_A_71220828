f42bdca8928914f6809b0f9580fd5e97caf9c1d4
ivanconanta <118499597+ivanconanta@users.noreply.github.com>
2022-12-09 10:14:25 +0700

    Add files via upload

---
+++ 2_A_71220828.py
@@ -0,0 +1,10 @@
+#2.A.Test case 1
+a = str(input("Masukkan nama: "))
+b = 0
+for i in range(len(x)):
+    b = b + 1
+    print(x[:y])
+for i in range(len(x)):
+    b = b - 1
+    print(x[:y])
+
---
+++ 3_A_71220828.py
@@ -0,0 +1,17 @@
+#3.A.Test case 1
+a = int(input("Masukkan Pembatas: "))
+b = int(input("Masukkan Angka  yang dilarang: "))
+for i in range(a):
+    if i == b:
+        continue
+    else:
+        print(i)
+
+#3.B.Test case 2
+a = int(input("Masukkan Pembatas: "))
+b = int(input("Masukkan Angka  yang dilarang: "))
+for i in range(a):
+    if i == b:
+        continue
+    else:
+        print(i)
